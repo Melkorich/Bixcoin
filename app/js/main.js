5361246f494a71985989497f1376c57f76134f7e
Melkorich <Kirill.Lavrov92@gmail.com>
2021-12-13 02:10:01 +0300

    section about, market, how it works added

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,12 @@
 $(function(){
 
-  $('.menu__search .menu__btn').on('click', function() {
+  $(' .menu__btn').on('click', function() {
     $('.search-txt').toggleClass('search-text--active');
   });
 
   $('.menu__link, .header__navigation-lin').on('click', function() {
     $('.search-txt').removeClass('search-text--active');
   });
+
  
 });
